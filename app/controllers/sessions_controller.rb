5102e7b84ce5359a710287f29013b2b79d53b0db
Gilbert Torchon <gilbert@noukod.com>
2019-01-30 18:05:52 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,9 @@ class SessionsController < ApplicationController
 
   def create
     if params[:name].nil? || params[:name].empty?
-      redirect_to "/login"
+      redirect_to "/login" 
+    else 
+      session[:name]=params[:name]
     end
   end
 
